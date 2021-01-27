74ba2ca118ddd760766731da63d4d79d6e1b104f
PhilipDeFraties <Philipjfry338@gmail.com>
2021-01-27 13:47:47 -0700

    first commit

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      connection: 'postgres://localhost/publications',
+      migrations: {
+        directory: './db/migrations'
+      },
+      seeds: {
+        directory: './db/seeds/dev'
+      },
+      useNullAsDefault: true
+    }
+  }
+};
